fcadaa6037f97c8dfbf83cdee4fb3b12fe6d9853
Gurkaran Singh <sgurkaran2000@gmail.com>
2020-01-10 14:00:41 +0530

    ...

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const axios = require('axios') 
+const axios = require('axios')
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 }, () => {
+const wss = new WebSocket.Server({
+    port: 8080
+}, () => {
     console.log("started")
 });
 var users = []
 wss.on('connection', (socket, req) => {
-    console.log(req.url+":[Connected]")
+    console.log(req.url + ":[Connected]")
     users[req.url] = socket
     socket.on('message', (data) => {
-        console.log(req.url+":"+data) 
-    if(users[JSON.parse(data).receiver] != null){
-        users[JSON.parse(data).receiver].send(data)
-        }else{
-            axios.post("http://localhost:2643/save-message",{
-                data:data,
-            })
-	    }
+        console.log(req.url + ":" + data)
+        if (users[JSON.parse(data).receiver] != null) {
+            users[JSON.parse(data).receiver].send(data)
+        }
+        // else {
+        //     axios.post("http://localhost:2643/save-message", {
+        //         data: data,
+        //     })
+        // }
     })
-}); 
+});
