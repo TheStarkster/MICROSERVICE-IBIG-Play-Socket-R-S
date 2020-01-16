baeb48af2a69a37490cdaccd7e9408337d65e950
Gurkaran Singh <sgurkaran2000@gmail.com>
2020-01-17 02:42:50 +0530

    ...

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ wss.on('connection', (socket, req) => {
             console.log(JSON.parse(JSON.parse(data).participants_phone))
             console.log(typeof(JSON.parse(JSON.parse(data).participants_phone)))
             JSON.parse(JSON.parse(data).participants_phone).forEach(element => {
-                if (users["/"+element] != null && users["/"+element] != "/"+JSON.parse(data).sender_phone) {
+                if (users["/"+element] != null && "/"+element != "/"+JSON.parse(data).sender_phone) {
                     console.log("------------------")
                     console.log("/"+element)
                     console.log("/"+JSON.parse(data).sender_phone)
